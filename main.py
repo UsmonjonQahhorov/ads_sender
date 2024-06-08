e8b25b94530068ac8d7dfd443957774aa1c71150
usmonjonqahhorov <qohhorovusmonjon@gmail.com>
2024-06-08 09:52:55 +0500

    updates

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 logging.basicConfig(level=logging.INFO)
 scheduler = AsyncIOScheduler()
 
-# async def create_all():
-#     await db.create_all()
-#     await Users().create()
-#     await Groups().create()
-#     await Messages().create()
+async def create_all():
+    await db.create_all()
+    await Users().create()
+    await Groups().create()
+    await Messages().create()
 
 if __name__ == '__main__':
     db.init()
     scheduler.start()
     executor.start_polling(dp, skip_updates=True)
     loop = get_event_loop()
-    # loop.run_until_complete(create_all())
+    loop.run_until_complete(create_all())
     logging.getLogger('apscheduler').setLevel(logging.DEBUG)
     loop.run_forever()
